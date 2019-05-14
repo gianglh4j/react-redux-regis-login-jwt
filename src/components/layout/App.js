d42c6113b5bc938456d0e460e8a9477bdeeb09f2
gianglh4j <gianglh4j@gmail.com>
2019-05-15 00:10:08 +0700

    link redux with react component , create privateRouter

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -8,6 +8,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../store.js";
 
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../../utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "../../actions/authActions";
+
+// Check for token to keep user logged in/ xet khi load lai trang 
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
 
 class App extends Component {
   constructor(props) {
--- src/components/layout/body/Body.jsx
+++ src/components/layout/body/Body.jsx
@@ -5,6 +5,9 @@ import ShopPage from "../../pages/ShopPage";
 import Login from "../../auth/Login";
 import Register from "../../auth/Register";
 
+import PrivateRoute from "../../private-route/PrivateRoute";
+import Dashboard from "../../dashboard/Dashboard";
+
 import("./Body.css")
   .then()
   .catch(err => console.log(err));
@@ -19,6 +22,7 @@ export default class Body extends Component {
           <Route exact path="/shop" component={ShopPage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
           <Route exact path="/jquery" render={() => <div>jquery</div>} />
           <Route exact path="/android" render={() => <div>android</div>} />
           <Route render={() => <div>404 Page Not Found</div>} />
